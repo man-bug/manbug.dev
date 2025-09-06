7bad64db792fd1c204314354940fe5b2baca759b
man-bug <damianoconnor04@gmail.com>
2025-09-06 19:07:35 -0400

    actually fix flashing

--- apps/www/src/app/providers.tsx
+++ apps/www/src/app/providers.tsx
@@ -4,6 +4,9 @@ import * as React from "react";
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
+// https://github.com/pacocoursey/next-themes/pull/354
+const fixedThemeBlindScript = `((e,t,s,l,c,a,r,i)=>{let m=document.documentElement,n=["light","dark"];function h(t){let s=Array.isArray(e)?e:[e];s.forEach(e=>{let s="class"===e,l=s&&a?c.map(e=>a[e]||e):c;s?(m.classList.remove(...l),m.classList.add(a&&a[t]?a[t]:t)):m.setAttribute(e,t)}),o(t)}function o(e){i&&n.includes(e)&&(m.style.colorScheme=e)}function d(){return window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"light"}if(l)h(l);else try{let u=localStorage.getItem(t)||s,f=r&&"system"===u,y=f?d():u;h(y)}catch(g){}})("class","theme","system",null,["light","dark"],null,!0,!0);`;
+
 export function Providers({ children }: { children: React.ReactNode }) {
     return (
         <NextThemesProvider
@@ -15,6 +18,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
             // https://github.com/pacocoursey/next-themes?tab=readme-ov-file#using-with-cloudflare-rocket-loader
             scriptProps={{ "data-cfasync": "false" }}
         >
+            <script>{fixedThemeBlindScript}</script>
             {children}
         </NextThemesProvider>
     );
